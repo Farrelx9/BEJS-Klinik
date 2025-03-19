ffdff43cad6b220a968fb66333ea76f98e21526e
Farrelll <137156152+Farrelx9@users.noreply.github.com>
2025-03-19 19:17:36 +0700

    upd prisma postgres

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+require("dotenv").config();
 
-var indexRouter = require("./routes/index");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cors = require("cors");
 
-var app = express();
+const routes = require("./routes");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(routes);
 
-app.use("/", indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.get("/", (req, res) => {
+  res.send(`<h1 align="center">Welcome To API Cahaya Kreativ</h1>`);
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+// 404 error handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: false,
+    message: `are you lost? ${req.method} ${req.url} is not registered!`,
+    data: null,
+  });
 });
-app.listen(3000, () => console.log("app listening on port 3000!"));
+
+// 500 error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    status: false,
+    message: err.message,
+    data: null,
+  });
+});
+
+app.listen(PORT, () => console.log("Listening on port :", PORT));
 
 module.exports = app;
