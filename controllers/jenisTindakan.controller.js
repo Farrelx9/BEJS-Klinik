001f18661e1bf2062a20306e08e2a6f9e7604165
Farrelll <137156152+Farrelx9@users.noreply.github.com>
2025-06-09 06:09:40 +0700

    Update jenisTindakan.controller.js

--- controllers/jenisTindakan.controller.js
+++ controllers/jenisTindakan.controller.js
@@ -33,7 +33,7 @@ exports.createJenisTindakan = async (req, res) => {
 };
 // 2. Ambil semua jenis tindakan
 exports.getAllJenisTindakan = async (req, res) => {
-  const { page = 1, limit = 5, search } = req.query;
+  const { page = 1, limit = 10, search } = req.query;
 
   try {
     // Validasi dan parsing parameter halaman & limit
@@ -70,7 +70,7 @@ exports.getAllJenisTindakan = async (req, res) => {
         createdAt: "desc", // <-- URUTKAN BERDASARKAN CREATED AT TERBARU
       },
     });
-    
+
     // Hitung metadata pagination
     const meta = getPaginationMeta(totalItems, take, pageNumber);
 
