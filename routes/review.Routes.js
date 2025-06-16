79feb1aa5f59899867e4c474089d05b78874c46c
Farrelll <137156152+Farrelx9@users.noreply.github.com>
2025-06-17 02:03:47 +0700

    Update review.Routes.js

--- routes/review.Routes.js
+++ routes/review.Routes.js
@@ -5,21 +5,17 @@ const review = require("../controllers/review.Controller");
 const authMiddleware = require("../middlewares/auth");
 
 // Create a new review
-router.post("/reviews", authMiddleware, review.createReview);
+router.post("/", authMiddleware, review.createReview);
 
 // Get all reviews
-router.get("/reviews", authMiddleware, review.getAllReviews);
+router.get("/", authMiddleware, review.getAllReviews);
 
 // Get reviews by patient ID
-router.get(
-  "/reviews/patient/:id_pasien",
-  authMiddleware,
-  review.getReviewsByPatientId
-);
+router.get("/patient/:id_pasien", authMiddleware, review.getReviewsByPatientId);
 
 // Get average rating by patient ID
 router.get(
-  "/reviews/patient/:id_pasien/average-rating",
+  "/patient/:id_pasien/average-rating",
   authMiddleware,
   review.getAverageRatingByPatientId
 );
